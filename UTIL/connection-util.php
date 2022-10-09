7ca50ba7798fe184d3cefbf5f09e346446b6cbaf
vinaanjelina <71738548+vinaanjelina@users.noreply.github.com>
2022-10-09 22:08:37 +0700

    Util
    PDO Connection

---
+++ UTIL/connection-util.php
@@ -0,0 +1,7 @@
+<?php
+function getMySQLConnection() {
+    $link = new PDO('mysql:host=localhost;dbname=attendancedb', 'root', ' ');
+    $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
+    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $link;
+}
