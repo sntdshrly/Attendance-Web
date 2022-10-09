b2d3cb1a2ecf13cefe548fac781dc5b50d159b8c
vinaanjelina <71738548+vinaanjelina@users.noreply.github.com>
2022-10-09 22:11:15 +0700

    Delete UTIL directory

--- UTIL/connection-util.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getMySQLConnection() {
-    $link = new PDO('mysql:host=localhost;dbname=attendancedb', 'root', ' ');
-    $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
-    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $link;
-}
