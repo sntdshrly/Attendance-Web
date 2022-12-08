04ffa2298d745b509b33db1bce969d243d34c4bb
sherly santiadi <sntdshrly@gmail.com>
2022-12-08 09:54:37 +0700

    rapiin

--- controller/ProdiController.php
+++ controller/ProdiController.php
@@ -12,59 +12,60 @@ class ProdiController
     {
         /* fungsi delete prodi */
         $deleteApproved = filter_input(INPUT_GET, 'delcomProdi');
-        if(isset($deleteApproved)&&$deleteApproved==1){
-            $deletedId = filter_input(INPUT_GET,'didProdi');
+        if (isset($deleteApproved) && $deleteApproved == 1) {
+            $deletedId = filter_input(INPUT_GET, 'didProdi');
             $result = $this->prodiDao->deleteProdi($deletedId);
         }
         $prodi = $this->prodiDao->fetchAllProdi();
         include_once 'view/prodi-view.php';
     }
 
-     public function addProdi(){
-         $buttonPressed = filter_input(INPUT_POST,'btnAddProdi');
-         if(isset($buttonPressed)){
-             var_dump("test");
-             $idProdi = filter_input(INPUT_POST,'idProdi');
-             $namaProdi = filter_input(INPUT_POST,'namaProdi');
-             $tingkatanProdi = filter_input(INPUT_POST,'tingkatanProdi');
+    public function addProdi()
+    {
+        $buttonPressed = filter_input(INPUT_POST, 'btnAddProdi');
+        if (isset($buttonPressed)) {
+            var_dump("test");
+            $idProdi = filter_input(INPUT_POST, 'idProdi');
+            $namaProdi = filter_input(INPUT_POST, 'namaProdi');
+            $tingkatanProdi = filter_input(INPUT_POST, 'tingkatanProdi');
 
-             $trimmedId = trim($idProdi);
-             $trimmedNama = trim($namaProdi);
-             $trimmedTingkatan = trim($tingkatanProdi);
+            $trimmedId = trim($idProdi);
+            $trimmedNama = trim($namaProdi);
+            $trimmedTingkatan = trim($tingkatanProdi);
 
-             $prodi = new Prodi;
-             $prodi->setIdProdi($trimmedId);
-             $prodi->setNamaProdi($trimmedNama);
-             $prodi->setTingkatanProdi($trimmedTingkatan);
+            $prodi = new Prodi;
+            $prodi->setIdProdi($trimmedId);
+            $prodi->setNamaProdi($trimmedNama);
+            $prodi->setTingkatanProdi($trimmedTingkatan);
 
-             $result = $this->prodiDao->saveProdi($prodi);
-         }
-         $prodi = $this->prodiDao->fetchAllProdi();
-         include_once 'view/prodi-form-view.php';
-     }
+            $result = $this->prodiDao->saveProdi($prodi);
+        }
+        $prodi = $this->prodiDao->fetchAllProdi();
+        include_once 'view/prodi-form-view.php';
+    }
 
-     public function updateProdi(){
-        $editedId = filter_input(INPUT_GET,'eidProdi');
-        if(isset($editedId) && $editedId != ''){
+    public function updateProdi()
+    {
+        $editedId = filter_input(INPUT_GET, 'eidProdi');
+        if (isset($editedId) && $editedId != '') {
             $prodi = $this->prodiDao->fetchProdiById($editedId);
         }
-        $buttonPressed = filter_input(INPUT_POST,'btnSubmit');
-        if(isset($buttonPressed)){
-            $namaProdi = filter_input(INPUT_POST,'namaProdi');
-            $tingkatanProdi = filter_input(INPUT_POST,'tingkatanProdi');
-    
+        $buttonPressed = filter_input(INPUT_POST, 'btnSubmit');
+        if (isset($buttonPressed)) {
+            $namaProdi = filter_input(INPUT_POST, 'namaProdi');
+            $tingkatanProdi = filter_input(INPUT_POST, 'tingkatanProdi');
+
             $updatedProdi = new Prodi();
             $updatedProdi->setIdProdi($prodi->getIdProdi());
             $updatedProdi->setNamaProdi($namaProdi);
             $updatedProdi->setTingkatanProdi($tingkatanProdi);
             $result = $this->prodiDao->updateProdi($updatedProdi);
-                if ($result){
-                    header('location:index.php?webmenu=prodi');
-                }
-                else{
-                    echo '<div class="bg-error">Failed to update</div>';
-                }
+            if ($result) {
+                header('location:index.php?webmenu=prodi');
+            } else {
+                echo '<div class="bg-error">Failed to update</div>';
             }
-            include_once 'view/update-prodi-view.php';
         }
+        include_once 'view/update-prodi-view.php';
+    }
 }
