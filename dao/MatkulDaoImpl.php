7d2b2b72130428c00aea67bc73c0e66086f5a992
vinaanjelina <71738548+vinaanjelina@users.noreply.github.com>
2023-01-19 15:55:50 +0700

    Update

--- dao/MatkulDaoImpl.php
+++ dao/MatkulDaoImpl.php
@@ -5,12 +5,23 @@ class MatkulDaoImpl
     public function fetchAllMatkul()
     {
         $link = ConnectionUtil::getMySQLConnection();
-        $query = 'SELECT p.id_prodi, p.nama_prodi AS "prodi_name", m.kode_mk, m.nama_mk, m.prodi_id_prodi, m.jumlah_sks FROM matkul m LEFT OUTER JOIN prodi p ON m.prodi_id_prodi = p.id_prodi';
+        $query = $query = 'SELECT p.id_prodi, p.nama_prodi AS "prodi_name", m.kode_mk, m.nama_mk, m.prodi_id_prodi, m.jumlah_sks FROM matkul m LEFT OUTER JOIN prodi p ON m.prodi_id_prodi = p.id_prodi';
         $stmt = $link->prepare($query);
         $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Matkul');
         $stmt->execute();
         $link = null;
         return $stmt->fetchAll();
+    }
+
+    public function fetchMatkulByKode($kode_mk) {
+        $link = ConnectionUtil::getMySQLConnection();
+        $query = 'SELECT * FROM matkul WHERE kode_mk = ?';
+        $stmt = $link->prepare($query);
+        $stmt->bindParam(1, $kode_mk);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        $stmt->execute();
+        $link = null;
+        return $stmt->fetchObject('Matkul');
     }
 
     public function saveMatkul(Matkul $matkul)
@@ -51,14 +62,14 @@ class MatkulDaoImpl
         return $result;
     }
 
-    public function updateMatkul(dosen $matkul) {
+    public function updateMatkul(matkul $matkul) {
         $result = 0;
         $link = ConnectionUtil::getMySQLConnection();
-        $query = 'UPDATE matkul SET nama_mk = ?, prodi_id_prodi = ?, jumlah_sks = ? WHERE kode_matkul = ?';
+        $query = 'UPDATE matkul SET nama_mk = ?, prodi_id_prodi = ?, jumlah_sks = ? WHERE kode_mk = ?';
         $stmt = $link->prepare($query);
         $stmt->bindValue(4, $matkul->getKodeMk());
         $stmt->bindValue(1, $matkul->getNamaMk());
-        $stmt->bindValue(2, $matkul->getProdi()->getNamaProdi());
+        $stmt->bindValue(2, $matkul->getProdi()->getIdProdi());
         $stmt->bindValue(3, $matkul->getJumlahSks());
         $link->beginTransaction();
         if ($stmt->execute()) {
@@ -69,16 +80,5 @@ class MatkulDaoImpl
         }
         $link = null;
         return $result;
-    }
-
-    public function fetchMatkulByKode($kode_matkul) {
-        $link = ConnectionUtil::getMySQLConnection();
-        $query = 'SELECT * FROM matkul WHERE kode_mk = ?';
-        $stmt = $link->prepare($query);
-        $stmt->bindParam(1, $kode_matkul);
-        $stmt->setFetchMode(PDO::FETCH_OBJ);
-        $stmt->execute();
-        $link = null;
-        return $stmt->fetchObject('Matkul');
     }
 }
