5884253761bd4fc770b8d18289d4766b73a6f65f
vinaanjelina <71738548+vinaanjelina@users.noreply.github.com>
2023-01-19 15:57:41 +0700

    Update update-matkul-view.php

--- view/update-matkul-view.php
+++ view/update-matkul-view.php
@@ -23,36 +23,33 @@
                         </select>
                     </div> -->
                     <div class="form-group">
-                        <label for="labelKode">NIK</label>
-                        <input type="text" id="idKodeMk" name="kodeMk" class="form-control" required
-                               value="<?php echo $matkul->getKodeMk(); ?>" readonly>
+                        <label for="labelKodeMatkul">Kode Matkul</label>
+                        <input type="text" id="idkodeMatkul" name="kodeMatkul" class="form-control" required>
                     </div>
                     <div class="form-group">
                         <label for="labelNamaMatkul">Nama Matkul</label>
-                        <input type="text" id="idNamaMatkul" name="namaMatkul" class="form-control"
-                               value="<?php echo $matkul->getNamaMk(); ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="labeljumlahSks">Jumlah SKS</label>
-                        <input type="text" id="idJumlahSks" name="jumlahSks" class="form-control"
-                               value="<?php echo $matkul->getJumlahSks(); ?>" required>
+                        <input type="text" id="idNamaMatkul" name="namaMatkul" class="form-control" required>
                     </div>
                     <div class="form-group">
                         <label for="labelProdi">Prodi</label>
                         <select class="form-control select2" style="width: 100%;" name="prodi" id="idProdi">
                             <?php
                             /**@var $item*/
-                            foreach($prodi as $item) {
-                                echo '<option>'.$item->getIdProdi().' - '.$item->getNamaProdi().'</option>';
+                            foreach ($prodi as $item) {
+                                echo '<option>' . $item->getIdProdi() . ' - ' . $item->getNamaProdi() . '</option>';
                             }
                             ?>
                         </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="labelJumlahSks">Jumlah SKS</label>
+                        <input type="text" id="idJumlahSks" name="jumlahSks" class="form-control" required>
                     </div>
                 </div>
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                    <button type="submit" name="btnUpdateMatkul" class="btn btn-primary">Submit</button>
+                    <button type="submit" name="btnAddMatkul" class="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
